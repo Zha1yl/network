002d33bf5e6d034bd747711bbcd04b4aefbcf990
Zhaiyl Mamyraliev <mamyraliev59@gmail.com>
2024-02-19 13:23:51 +0600

    cart

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -1,1 +1,6 @@
 export const API = "http://localhost:8000/products";
+export const API_CATEGORIES = "http://localhost:8000/categories";
+export const ACTIONS = {
+  GET_CART: "GET_CART",
+  GET_CATEGORIES: "GET_CATEGORIES",
+};
