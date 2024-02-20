787de11e24ac3dac4644356593432e3abdd826cc
Zhaiyl Mamyraliev <mamyraliev59@gmail.com>
2024-02-20 15:54:58 +0600

    crudProduct

--- src/helpers/const.js
+++ src/helpers/const.js
@@ -2,8 +2,10 @@ export const API = "http://localhost:8000/products";
 export const API_POSTS = "http://localhost:8000/posts";
 export const API_CATEGORIES = "http://localhost:8000/categories";
 export const ACTIONS = {
+  GET_PRODUCTS: "GET_PRODUCTS",
+  GET_ONE_PRODUCTS: "GET_ONE_PRODUCTS",
   GET_POSTS: "GET_POSTS",
   GET_ONE_POST: "GET_ONE_POST",
   GET_CART: "GET_CART",
   GET_CATEGORIES: "GET_CATEGORIES",
-}
+};
